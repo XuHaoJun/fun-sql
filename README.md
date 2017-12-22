f25353d766ce97d1d01ed75fbe041ea7a3ff6b16
XuHaoJun <XuhaojunTW@gmail.com>
2017-12-23 02:06:32 +0800

    Fix README typo.

--- README.md
+++ README.md
@@ -9,48 +9,66 @@ For fun with SQL.
 ## Example
 
 ```javascript
-const funSqlBuild = require("fun-sql/build");
+const funSqlBuild = require('fun-sql/build');
 
-const query = funSqlBuild((funSql) => {
-    const BookSet = funSql.table("books");
-    const Book = BookSet.fields;
+const query = funSqlBuild(funSql => {
+  const BookSet = funSql.table('books');
+  const Book = BookSet.fields;
 
-    const AuthorSet = funSql.table("authors");
-    const Author = AuthorSet.fields;
+  const AuthorSet = funSql.table('authors');
+  const Author = AuthorSet.fields;
 
-    const BookAuthorSet = funSql.table("bookAuthors");
-    const BookAuthor = BookAuthorSet.fields;
+  const BookAuthorSet = funSql.table('bookAuthors');
+  const BookAuthor = BookAuthorSet.fields;
 
-    const joined = BooksSet
-        .intersection(BookAuthorSet, () => {
-            return Book.id === BookAuthor.bookId;
-        })
-        .intersection(AuthorSet, () => {
-            return BookAuthor.authorId === Author.id;
-        });
-    // const leftoined = Books.intersection(Authors, () => ...).union(Authors);
-    // const rightJoined = Books.intersection(Authors, () => ...).union(Authors);
-    // const fullJoined = Books.union(Authors);
+  const joined = BooksSet.intersection(BookAuthorSet, () => {
+    return Book.id === BookAuthor.bookId;
+  }).intersection(AuthorSet, () => {
+    return BookAuthor.authorId === Author.id;
+  });
+  // const leftoined = Books.intersection(Authors, () => ...).union(Authors);
+  // const rightJoined = Books.intersection(Authors, () => ...).union(Authors);
+  // const fullJoined = Books.union(Authors);
 
-    const grouped = joined.groupBy(Author.id);
+  const filted = joined.filter(() => {
+    return Book.sales > 100 && funSql.len(Book.title) > 7;
+  });
 
-    const mapped = grouped.map(() => {
-        const bookId = Book.id;
-        const bookTitle = Book.title;
-        const authorName = Author.name;
-        const totalSales = funSql.sum(Book.sales);
-        const avgSales = funSql.avg(Book.sales);
-        return { bookId, bookTitle, authorId, authorName, totalSales, avgSales };
-    });
+  const grouped = filted.groupBy(Author.id);
 
-    const Mapped = mapped.fields;
+  const mapped = grouped.map(() => {
+    const bookId = Book.id;
+    const bookTitle = Book.title;
+    const authorName = Author.name;
+    const totalSales = funSql.sum(Book.sales);
+    let range;
+    if (totalSales === 0) {
+      range = 'none';
+    } else if (totalSales > 1000) {
+      range = 'good';
+    } else {
+      range = 'normal';
+    }
+    const avgSales = funSql.avg(Book.sales);
+    return {
+      bookId,
+      bookTitle,
+      authorId,
+      authorName,
+      totalSales,
+      avgSales,
+      range
+    };
+  });
 
-    const ordered = mapped.orderBy(
-                    { field: Mapped.totalSales, sort: "desc" },
-                    { field: Book.createdAt }
-                  );
+  const Mapped = mapped.fields;
 
-    return ordered;
+  const ordered = mapped.orderBy(
+    { field: Mapped.totalSales, sort: 'desc' },
+    { field: Mapped.authorName }
+  );
+
+  return ordered;
 });
 
 console.log(query.toString());
@@ -59,33 +77,40 @@ console.log(query.toString());
 //        Author.name as authorName,
 //        SUM(Book.sales) as totalSales,
 //        AVG(Book.sales) as avgSales,
+//        CASE WHEN totalSales = 0 THEN 'none'
+//             WHEN totalSales > 1000 THEN 'good'
+//             ELSE 'normal' AS range
 // FROM books as Book
 // INNER JOIN bookAuthors as BookAuthor ON Book.id = BookAuthor.bookId
 // INNER JOIN authors as Author ON BookAuthor.authorId = Author.id
+// WHERE Book.sales > 100 and LEN(Book.title) > 7
 // GROUP BY Author.id
-// ORDER BY totalSales
+// ORDER BY totalSales DESC, authorName
 
-
-const funSqlTestExec = require("fun-sql/testExec");
+const funSqlTestExec = require('fun-sql/testExec');
 const dataSets = {
-    books: [
-        {id: 1, title: "Structure and Interpretation of Computer Programs", sales: 5169},
-        {id: 2, title: "Hacking the Xbox", sales: 574},
-    ],
-    bookAuthors: [
-        {bookId: 1, authorId: 1},
-        {bookId: 1, authorId: 2},
-        {bookId: 1, authorId: 3},
-        {bookId: 2, authorId: 1},
-    ],
-    authors: [
-        {id: 1, name: "Harold Abelson"},
-        {id: 2, name: "Gerald Jay Sussman"},
-        {id: 3, name: "Julie Sussman"},
-    ]
+  books: [
+    {
+      id: 1,
+      title: 'Structure and Interpretation of Computer Programs',
+      sales: 5169
+    },
+    { id: 2, title: 'Hacking the Xbox', sales: 574 }
+  ],
+  bookAuthors: [
+    { bookId: 1, authorId: 1 },
+    { bookId: 1, authorId: 2 },
+    { bookId: 1, authorId: 3 },
+    { bookId: 2, authorId: 1 }
+  ],
+  authors: [
+    { id: 1, name: 'Harold Abelson' },
+    { id: 2, name: 'Gerald Jay Sussman' },
+    { id: 3, name: 'Julie Sussman' }
+  ]
 };
 
-const rows = funSqlTestExec({dataSets: dataSets, query: query2});
+const rows = funSqlTestExec({ dataSets: dataSets, query: query2 });
 console.log(rows);
 // [
 //   {
--- example.js
+++ example.js
@@ -1,99 +1,105 @@
-const funSqlBuild = require("fun-sql/build");
+const funSqlBuild = require('fun-sql/build');
 
-const query = funSqlBuild((funSql) => {
-	const BookSet = funSql.table("books");
-	const Book = BookSet.fields;
-	const filted = Books.filter(() => { 
-		const { price, amount, updatedAt } = Book;
-		return price > 100 && amount > 0 && updatedAt !== null;
-	});
-	return filted.map(() => { 
-		const { id, prevEditionId } = Book;
-		const colName = prevEditionId > 1 ? prevEditionId : id;
-		return {id, colName};
-	});
+const query = funSqlBuild(funSql => {
+  const BookSet = funSql.table('books');
+  const Book = BookSet.fields;
+
+  const AuthorSet = funSql.table('authors');
+  const Author = AuthorSet.fields;
+
+  const BookAuthorSet = funSql.table('bookAuthors');
+  const BookAuthor = BookAuthorSet.fields;
+
+  const joined = BooksSet.intersection(BookAuthorSet, () => {
+    return Book.id === BookAuthor.bookId;
+  }).intersection(AuthorSet, () => {
+    return BookAuthor.authorId === Author.id;
+  });
+  // const leftoined = Books.intersection(Authors, () => ...).union(Authors);
+  // const rightJoined = Books.intersection(Authors, () => ...).union(Authors);
+  // const fullJoined = Books.union(Authors);
+
+  const filted = joined.filter(() => {
+    return Book.sales > 100 && funSql.len(Book.title) > 7;
+  });
+
+  const grouped = filted.groupBy(Author.id);
+
+  const mapped = grouped.map(() => {
+    const bookId = Book.id;
+    const bookTitle = Book.title;
+    const authorName = Author.name;
+    const totalSales = funSql.sum(Book.sales);
+    let range;
+    if (totalSales === 0) {
+      range = 'none';
+    } else if (totalSales > 1000) {
+      range = 'good';
+    } else {
+      range = 'normal';
+    }
+    const avgSales = funSql.avg(Book.sales);
+    return {
+      bookId,
+      bookTitle,
+      authorId,
+      authorName,
+      totalSales,
+      avgSales,
+      range
+    };
+  });
+
+  const Mapped = mapped.fields;
+
+  const ordered = mapped.orderBy(
+    { field: Mapped.totalSales, sort: 'desc' },
+    { field: Mapped.authorName }
+  );
+
+  return ordered;
 });
 
 console.log(query.toString());
-// SELECT Book.id as id,
-//        CASE WHEN Book.prevEditionId > 1 THEN Book.prevEditionId ELSE Book.id END AS colName
-// FROM books as Book
-// WHERE Book.price > 100 and Book.amount > 0 and Book.updatedAt IS NOT NULL
-
-
-const query2 = funSqlBuild((funSql) => {
-	const BookSet = funSql.table("books");
-	const Book = BookSet.fields;
-
-	const AuthorSet = funSql.table("authors");
-	const Author = AuthorSet.fields;
-
-	const BookAuthorSet = funSql.table("bookAuthors");
-	const BookAuthor = BookAuthorSet.fields;
-
-	const joined = BooksSet
-		.intersection(BookAuthorSet, () => {
-			return Book.id === BookAuthor.bookId;
-		})
-		.intersection(AuthorSet, () => {
-			return BookAuthor.authorId === Author.id;
-		});
-	// const leftoined = Books.intersection(Authors, () => ...).union(Authors);
-	// const rightJoined = Books.intersection(Authors, () => ...).union(Authors);
-	// const fullJoined = Books.union(Authors); 
-	
-	const grouped = joined.groupBy(Author.id);
-
-	const mapped = grouped.map(() => { 
-		const bookId = Book.id;
-		const bookTitle = Book.title;
-		const authorName = Author.name;
-		const totalSales = funSql.sum(Book.sales);
-		const avgSales = funSql.avg(Book.sales);
-		return { bookId, bookTitle, authorId, authorName, totalSales, avgSales };
-	});
-
-	const Mapped = mapped.fields;
-
-	const ordered = mapped.orderBy({ field: Mapped.totalSales, sort: "desc" },
-					{ field: Mapped.authorName });
-
-	return ordered;
-});
-
-console.log(query2.toString());
 // SELECT Book.authorId as authorId,
 //        Book.id as bookId,
 //        Author.name as authorName,
 //        SUM(Book.sales) as totalSales,
-//        SUM(Book.sales) as avgSales,
+//        AVG(Book.sales) as avgSales,
+//        CASE WHEN totalSales = 0 THEN 'none'
+//             WHEN totalSales > 1000 THEN 'good'
+//             ELSE 'normal' AS range
 // FROM books as Book
 // INNER JOIN bookAuthors as BookAuthor ON Book.id = BookAuthor.bookId
 // INNER JOIN authors as Author ON BookAuthor.authorId = Author.id
+// WHERE Book.sales > 100 and LEN(Book.title) > 7
 // GROUP BY Author.id
-// ORDER BY totalSales
+// ORDER BY totalSales DESC, authorName
 
-
-const funSqlTestExec = require("fun-sql/testExec");
+const funSqlTestExec = require('fun-sql/testExec');
 const dataSets = {
-	books: [
-		{id: 1, title: "Structure and Interpretation of Computer Programs", sales: 5169},
-		{id: 2, title: "Hacking the Xbox", sales: 574},
-	],
-	bookAuthors: [
-		{bookId: 1, authorId: 1},
-		{bookId: 1, authorId: 2},
-		{bookId: 1, authorId: 3},
-		{bookId: 2, authorId: 1},
-	],
-	authors: [
-		{id: 1, name: "Harold Abelson"},
-		{id: 2, name: "Gerald Jay Sussman"},
-		{id: 3, name: "Julie Sussman"},
-	]
+  books: [
+    {
+      id: 1,
+      title: 'Structure and Interpretation of Computer Programs',
+      sales: 5169
+    },
+    { id: 2, title: 'Hacking the Xbox', sales: 574 }
+  ],
+  bookAuthors: [
+    { bookId: 1, authorId: 1 },
+    { bookId: 1, authorId: 2 },
+    { bookId: 1, authorId: 3 },
+    { bookId: 2, authorId: 1 }
+  ],
+  authors: [
+    { id: 1, name: 'Harold Abelson' },
+    { id: 2, name: 'Gerald Jay Sussman' },
+    { id: 3, name: 'Julie Sussman' }
+  ]
 };
 
-const rows = funSqlTestExec({dataSets: dataSets, query: query2);
+const rows = funSqlTestExec({ dataSets: dataSets, query: query2 });
 console.log(rows);
 // [
 //   {
